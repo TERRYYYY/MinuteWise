cf184d3febd27947dda7bdd18f55f377fd0ba832
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-01 17:18:11 +0300

    implementing template inheritance

--- app/views.py
+++ app/views.py
@@ -8,7 +8,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    
+    title = 'Moment in Time'
+    return render_template('index.html', title = title)
 
 @app.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):

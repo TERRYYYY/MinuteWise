39f3bbe4cdb9578fa4543873816f5fa49af0c051
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-04 12:40:57 +0300

    creating the user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
+        
 class Pitch:
 
     all_pitches = []

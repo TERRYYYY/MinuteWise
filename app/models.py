cf457144340118a05c3f545606e48e7d7572f4fe
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-04 13:53:27 +0300

    creating a one to many relationship

--- app/models.py
+++ app/models.py
@@ -2,36 +2,74 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
     def __repr__(self):
         return f'User {self.username}'
 
-class Pitch:
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
 
-    all_pitches = []
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'pitch',lazy="dynamic")
 
-    def __init__(self,movie_id,title,imageurl,review):
-        self.movie_id = movie_id
-        self.title = title
-        self.imageurl = imageurl
-        self.review = review
-
-
-    def save_pitch(self):
-        Review.all_pitches.append(self)
+    def __repr__(self):
+        return f'User {self.name}'
 
 
-    @classmethod
-    def clear_pitch(cls):
-        Pitch.all_pitches.clear()
 
-    @classmethod
-    def get_pitch(cls,id):
 
-        response = []
 
-        for pitch in cls.all_pitches:
-            if pitch.movie_id == id:
-                response.append(review)
 
-        return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Pitch:
+
+#     all_pitches = []
+
+#     def __init__(self,movie_id,title,imageurl,review):
+#         self.movie_id = movie_id
+#         self.title = title
+#         self.imageurl = imageurl
+#         self.review = review
+
+
+#     def save_pitch(self):
+#         Review.all_pitches.append(self)
+
+
+#     @classmethod
+#     def clear_pitch(cls):
+#         Pitch.all_pitches.clear()
+
+#     @classmethod
+#     def get_pitch(cls,id):
+
+#         response = []
+
+#         for pitch in cls.all_pitches:
+#             if pitch.movie_id == id:
+#                 response.append(review)
+
+#         return response
--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User
+from app.models import User,Pitch
 
 from flask_script import Manager,Server
 
@@ -12,6 +13,6 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Pitch = Pitch )
 if __name__ == '__main__':
     manager.run()

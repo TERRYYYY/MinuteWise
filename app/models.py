66911166031dd521464e0928a1bbbeb54cce00c2
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-05 10:56:48 +0300

    create a Login form that will give users access to some of the features of our application

--- app/models.py
+++ app/models.py
@@ -17,16 +17,16 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
 
     @property
-        def password(self):
-            raise AttributeError('You cannot read the password attribute')
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
 
-        @password.setter
-        def password(self, password):
-            self.pass_secure = generate_password_hash(password)
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
 
 
-        def verify_password(self,password):
-            return check_password_hash(self.pass_secure,password)
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'

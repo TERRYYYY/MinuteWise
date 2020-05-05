00db3fa4929297cc04ac8a3263624e1d8d2659fe
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-05 10:10:53 +0300

    using the flask-migrate extension to create database migrations

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from .forms import PitchForm
+# from .forms import PitchForm
 from ..models import Pitch
 
 @main.route('/')

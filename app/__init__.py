a4237f891bb00b1d0875e4601f3e8ffc66ebbe62
TERRYYYY <kariuki.terry11@gmail.com>
2020-05-05 10:16:14 +0300

    securing passwords

--- app/__init__.py
+++ app/__init__.py
@@ -20,9 +20,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
-    # from .request import configure_request
-    # configure_request(app)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
 
     return app
